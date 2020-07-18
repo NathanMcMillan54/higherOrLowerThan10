0c25c5b7570cb652b75e3a02512ec4369ad94037
NathanMcMillan54 <68480633+NathanMcMillan54@users.noreply.github.com>
2020-07-18 18:55:52 -0400

    Create main.c
    This file lets the user know if a number is higher or lower than 10

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+
+    printf("Enter an number: ");
+    scanf("%d", &number);
+
+    if (number > 10) {
+        printf("This number is higher than 10 \n");
+        printf("You entered %d.\n", number);
+    } else {
+        printf("This is lower than 10 \n");
+    }
+    return 0;
+}
