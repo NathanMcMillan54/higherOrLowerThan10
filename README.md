d8ef62dc7995dcf423e720599077fe0fdc38d153
Nathan McMillan <nathanmcmillan54@gmail.com>
2020-07-27 09:55:09 -0400

    Just decided to come back to this for some reason, trying to make main.c file run again after it's done runnign everything.

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ This just tells you if a number is higher or lower than 10 in C.
 sh compileRun.sh
 ```
 
-compile&Run.sh compiles 'main.c' and the runs 'main'.
+compile&Run.sh compiles 'main.c' and the runs file 'main'.
--- main.c
+++ main.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     int number;
 
     printf("Enter an number: ");
     scanf("%d", &number);
+    printf("You entered %d.\n", number);
 
     if (number > 10) {
         printf("This number is higher than 10 \n");
-        printf("You entered %d.\n", number);
     } else {
         printf("This is lower than 10 \n");
     }
-    printf("Type: ./main to restart \n");
+
+    char question;
+    printf("Would like do run this file again?");
+    scanf("%s", &question);
+    printf("You entered %d\n", question);
+    if (question == 'Yes') {
+        system("./main");
+    } else {
+        printf("Ok \n");
+    }
     return 0;
 }
